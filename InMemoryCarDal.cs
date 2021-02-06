fd9c02b994b45c5d0c45b80bf60b46008c214314
Zeyneptaskin03 <77525793+Zeyneptaskin03@users.noreply.github.com>
2021-02-06 17:55:49 +0300

    Add files via upload

---
+++ InMemoryCarDal.cs
@@ -0,0 +1,56 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+            new Car { Id = 1, BrandId = "AudiA1", ColorId = "01mavi", DailyPrice = 6, Description = "önden çekişli", ModelYear = 2020 },
+            new Car { Id = 2, BrandId = "OpelO2", ColorId = "02beyaz", DailyPrice = 70, Description = "arkadan itişli", ModelYear = 2010 },
+            new Car { Id = 3, BrandId = "BMW07", ColorId = "03siyah", DailyPrice = 63, Description = "manuel", ModelYear = 2021 }
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c => c.Id == Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.Description = car.Description;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+        }
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Id);
+                Console.WriteLine(car.BrandId);
+                Console.WriteLine(car.ColorId);
+                Console.WriteLine(car.DailyPrice);
+                Console.WriteLine(car.Description);
+                Console.WriteLine(car.ModelYear);
+                Console.WriteLine("---------------");
+            }
+        }
+    }
+}
